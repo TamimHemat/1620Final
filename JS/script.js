1e2d5e383791bb106ef250b3c2603d2eb3baf200
Tamim <tamimyasuo@gmail.com>
2021-12-10 13:03:40 -0800

    Fixed javascript for the dark mode and a little bit of the css. Now the toggle button properly remains on the page and changes it's background color. Before the fix the button style would disappear and wouldn't change background color. Now it looks much better

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,14 @@
 const tog = document.querySelector(".toggle")
-const main = document.querySelector(".light-theme")
 
 tog.addEventListener("click", (event) => {
     const root = document.querySelector(":root")
-    if (event.currentTarget.className == ".toggle") {    
-        event.currentTarget.className = ".untoggle"
-        root.style.setProperty("--white", "white")
-        root.style.setProperty("--black", "black")
-    } else {
-        event.currentTarget.className = ".toggle"
+    if (event.target.className == "toggle") {    
+        event.target.className = "untoggle"
         root.style.setProperty("--white", "black")
         root.style.setProperty("--black", "white")
+    } else {
+        event.target.className = "toggle"
+        root.style.setProperty("--white", "white")
+        root.style.setProperty("--black", "black")
     }
 })
