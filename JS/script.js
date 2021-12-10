0ee468887486a2bb7abba6c57bc24b3e821d5c5d
Tamim <tamimyasuo@gmail.com>
2021-12-10 13:31:56 -0800

    Added the form submit to log out the content of the name, email, and message

--- JS/script.js
+++ JS/script.js
@@ -6,9 +6,26 @@ tog.addEventListener("click", (event) => {
         event.target.className = "untoggle"
         root.style.setProperty("--white", "black")
         root.style.setProperty("--black", "white")
+        root.style.setProperty("--lightGreen", "#757E95")
     } else {
         event.target.className = "toggle"
         root.style.setProperty("--white", "white")
         root.style.setProperty("--black", "black")
+        root.style.setProperty("--lightGreen", "#BEE3DB")
     }
+})
+
+const submit = document.querySelector("button")
+
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    const obj = {}
+    const name = document.getElementById("name").innerHTML
+    const email = document.getElementById("email").innerHTML
+    const message = document.getElementById("msg").innerHTML
+    obj.name = name
+    obj.email = email
+    obj.message = message
+    console.log(obj)
 })
